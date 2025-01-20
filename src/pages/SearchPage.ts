711c42a3180d237469a1a734658bc28ff2fa7899
test-ilya <asdnone@yandex.ru>
2025-01-20 20:44:13 +0300

    modified:   src/pages/SearchPage.ts 	modified:   src/tests/searchInNewsBlock.test.ts

--- src/pages/SearchPage.ts
+++ src/pages/SearchPage.ts
@@ -2,22 +2,29 @@ import { Page } from '@playwright/test';
 
 export class SearchPage {
     private readonly inputSearchStringLocator = this.page.getByRole('textbox', { name: 'Поиск по сайту' });
-    public readonly searchResultsSelector = '[data-main-container]';
+    public readonly searchResultsSelector = (value: string) => 
+        this.page.getByRole('link', { name: value });
     private readonly blockNameSelector = (blockName: string) => 
         this.page.locator('#site-search-result-react-app').getByRole('link', { name: blockName });
 
-
     constructor(readonly page: Page) {};
 
+    /** Открыть страницу поиска */
     public async openSearchPage() {
         await this.page.goto('https://www.buhonline.ru/search');
     }
 
+    /** Выполнить поиск
+     * @param {string} value - Искомое значение
+    */
     public async searchTheSite(value: string) {
         await this.inputSearchStringLocator.fill(value);
         await this.page.keyboard.press('Enter');
     }
 
+    /**  Открыть блок в горизонтальном меню 
+     * @param {string} blockName - Наименование блока
+    */
     public async openBlockInMenu(blockName: string) {
         await this.blockNameSelector(blockName).click();
     }
--- src/tests/searchInNewsBlock.test.ts
+++ src/tests/searchInNewsBlock.test.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { SearchPage } from '../pages/SearchPage';
 
-const searchValue = 'праздники';
+const searchValue = 'Как оформить работу в выходные и праздники: разъяснение Роструда';
 const blockName = 'Новости';
 
 test('test', async ({ page }) => {
     const searchPage = new SearchPage(page);
-
+    //Открыть страницу поиска
     await searchPage.openSearchPage();
+    //Выполнить поиск по искомому значению
     await searchPage.searchTheSite(searchValue);
+    //Открыть блок в горизонтальном меню
     await searchPage.openBlockInMenu(blockName);
-
+    //Проверить, что на странице присуствует искомая статья
     await expect(
-        page.locator(searchPage.searchResultsSelector)
-    ).toContainText(searchValue);
-
+        searchPage.searchResultsSelector(searchValue)
+    ).toBeVisible();
 });
